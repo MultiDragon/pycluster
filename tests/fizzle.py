dff37afad892f9c21141faeaf0b0dc3ca9544404
MultiDragon <multidragon@yandex.ru>
2022-09-30 11:15:27 +0300

    proper tests for fizzle

--- tests/fizzle.py
+++ tests/fizzle.py
@@ -29,18 +29,36 @@ class ReplaceProxy(MessageObject):
         return 2 + value
 
 
+@registry.register(3)
+class FinalReplaceProxy(MessageObject):
+    def __init__(self, parent, **kwargs):
+        super().__init__(parent, **kwargs)
+        self.called = False
+
+    @replace("test")
+    def test(self, replace_target, value):
+        assert value == 6
+        self.called = True
+        return FizzleReplace
+
+
 def construct_tree():
     cluster = MessageCluster(registry)
     child1 = registry.create_and_insert(1, cluster, "child", cast_to=ReplaceTarget)
     child2 = registry.create_and_insert(2, cluster, "child2", cast_to=ReplaceProxy, ignored_value=6)
-    return cluster, child1, child2
+    child3 = registry.create_and_insert(3, cluster, "child3", cast_to=FinalReplaceProxy, ignored_value=6)
+    return cluster, child1, child2, child3
 
 
 def test_fizzle():
-    tree, child1, child2 = construct_tree()
+    tree, child1, child2, child3 = construct_tree()
     assert child2.new_value == -1
+    assert child1.test(5) == 7
+    assert child2.new_value == 5
+    assert not child3.called
     assert child1.test(6) == 7
-    assert child2.new_value == -1
+    assert child2.new_value == 5
+    assert child3.called
     assert child1.test(3) == 5
     assert child2.new_value == 3
 
